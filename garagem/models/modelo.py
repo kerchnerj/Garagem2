ac84d8e34f7c99d5a5faf09e37722622935ac670
eugreg <daiaegregory@gmail.com>
2023-08-16 16:13:35 -0300

    criacao da tabela modelo

---
+++ garagem/models/modelo.py
@@ -0,0 +1,14 @@
+from django.db import models
+from garagem.models import Categoria, Marca
+
+class Modelo(models.Model):
+    nome = models.CharField(max_length=100)
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="veiculos")
+    categoria = models.ForeignKey(
+        Categoria, on_delete=models.PROTECT, related_name="veiculos"
+    )
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name_plural = "nome"
--- garagem/models/veiculo.py
+++ garagem/models/veiculo.py
@@ -1,6 +1,7 @@
 from django.db import models
-from garagem.models import Categoria, Marca, Cor, Acessorio
+from garagem.models import Cor, Acessorio, Modelo
 from uploader.models import Image
+
 
 class Veiculo(models.Model):
     imagem = models.ForeignKey(
@@ -12,17 +13,17 @@ class Veiculo(models.Model):
         default=None,
     )
     modelo = models.CharField(max_length=100)
-    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="veiculos")
-    categoria = models.ForeignKey(
-        Categoria, on_delete=models.PROTECT, related_name="veiculos"
-    )
     cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
     ano = models.IntegerField(null=True, default=0)
+    modelo = models.ForeignKey(
+        Modelo, on_delete=models.PROTECT, related_name="veiculos"
+    )
+
     preco = models.DecimalField(max_digits=10, decimal_places=2, null=True, default=0)
     acessorio = models.ManyToManyField(Acessorio, related_name="Veiculo")
 
     def __str__(self):
-        return f"{self.marca}-{self.modelo}-{self.ano}-{self.cor}"
+        return f"{self.modelo}-{self.ano}-{self.cor}"
 
     class Meta:
         verbose_name = "veículo"
